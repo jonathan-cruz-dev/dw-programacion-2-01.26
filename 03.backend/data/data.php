e34d52a33b64f83d4cb99ed3b5db80ffe4f73e3e
jonathan-cruz-dev <136759463+jonathan-cruz-dev@users.noreply.github.com>
2026-05-04 10:02:27 -0300

    Arrays asociativos

---
+++ 03.backend/data/data.php
@@ -0,0 +1,17 @@
+<?php
+    $lista = [
+        [
+         'id' => 1, 
+         'titulo' => 'La revolución de la IA en la Programación', 
+         'foto'=>'images/ia.jpg'
+        ],
+        [
+         'id' => 2, 
+         'titulo' => 'Regreso de los Astronautas del Programa Artemis II', 'foto'=>'images/artemis.jpeg'
+        ],
+        [
+         'id' => 3,
+         'titulo' => 'Los astronautas de Artemis II no pisaron la Luna', 'foto'=>'images/artemis2.jpeg'
+        ]
+    ];
+?>
